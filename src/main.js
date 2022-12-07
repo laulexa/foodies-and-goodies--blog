2b028f04bac9ea5f95fb4ee0b66d2629693d1f34
laulexa <laura.alexandra.roa@gmail.com>
2022-12-06 22:02:09 -0800

    mobile navigation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 console.log("Hello Food lover!")
 
 iconMenuBtn.addEventListener('click', toggleMobileMenu);
+mobileBtn.addEventListener('click', closeMobileMenuContainer)
+mobileBtnA.addEventListener('click', closeMobileMenuContainer)
+mobileBtnR.addEventListener('click', closeMobileMenuContainer)
+mobileBtnAB.addEventListener('click', closeMobileMenuContainer)
 
 function toggleMobileMenu() {
    console.log("closing")
+ mobileMenu.classList.toggle('inactive')
+}
+
+function closeMobileMenuContainer () {
     mobileMenu.classList.toggle('inactive')
 }
+// desktopMenu.classList.toggle('inactive')
